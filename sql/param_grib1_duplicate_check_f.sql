f1483ef2f9fdd15f03e2542778b9a76b6c52fc02
Mikko Partio <mikko.partio@fmi.fi>
2019-10-03 12:11:42 +0300

    Specify schema

--- sql/param_grib1_duplicate_check_f.sql
+++ sql/param_grib1_duplicate_check_f.sql
@@ -6,7 +6,7 @@ AS $function$
 DECLARE
    _id int;
 BEGIN
-   SELECT id INTO _id FROM param_grib1 WHERE producer_id = NEW.producer_id AND param_id = NEW.param_id AND table_version = NEW.table_version AND timerange_indicator = NEW.timerange_indicator AND level_id IS NULL AND level_value IS NULL;
+   SELECT id INTO _id FROM public.param_grib1 WHERE producer_id = NEW.producer_id AND param_id = NEW.param_id AND table_version = NEW.table_version AND timerange_indicator = NEW.timerange_indicator AND level_id IS NULL AND level_value IS NULL;
 
    IF FOUND THEN
      RAISE EXCEPTION 'Row exists already for given input with id=%', _id;
--- sql/param_grib2_duplicate_check_f.sql
+++ sql/param_grib2_duplicate_check_f.sql
@@ -7,7 +7,7 @@ DECLARE
    _id int;
 BEGIN
   IF NEW.level_id IS NULL AND NEW.level_value IS NULL THEN
-    SELECT id INTO _id FROM param_grib2 WHERE producer_id = NEW.producer_id AND discipline = NEW.discipline AND category = NEW.category AND number = NEW.number AND type_of_statistical_processing = NEW.type_of_statistical_processing;
+    SELECT id INTO _id FROM public.param_grib2 WHERE producer_id = NEW.producer_id AND discipline = NEW.discipline AND category = NEW.category AND number = NEW.number AND type_of_statistical_processing = NEW.type_of_statistical_processing;
 
     IF FOUND THEN
       RAISE WARNING 'Row exists already for given input with id=%', _id;
