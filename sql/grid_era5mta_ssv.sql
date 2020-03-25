aabdefa65771f3f0b3b0d3192308d3d76f4e1bb8
Mikko Partio <mikko.partio@fmi.fi>
2020-03-25 10:44:46 +0200

    STU-12674: Update grid_era5mta_ssv view to automatically adjust for new year and disregard leap day

--- sql/grid_era5mta_ssv.sql
+++ sql/grid_era5mta_ssv.sql
@@ -1,10 +1,11 @@
 DROP VIEW grid_era5mta_ssv;
 
 CREATE VIEW grid_era5mta_ssv AS
+WITH raw AS (
 SELECT
   a.producer_id,
   f.name AS producer_name,
-  date_trunc('year', a.analysis_time + a.forecast_period) AS analysis_time,
+  date_trunc('year', (extract(year from current_date) - 2019) * interval '1 year' + a.analysis_time + a.forecast_period) AS analysis_time,
   a.geometry_id,
   g.name AS geometry_name,
   a.param_id,
@@ -15,9 +16,9 @@ SELECT
   a.level_value2,
   a.analysis_time AS orig_analysis_time,
   a.forecast_period AS orig_forecast_period,
-  a.analysis_time - date_trunc('year', a.analysis_time + a.forecast_period) + a.forecast_period AS forecast_period,
+  a.analysis_time + (extract(year from current_date) - 2019) * interval '1 year' - date_trunc('year', a.analysis_time + (extract(year from current_date) - 2019) * interval '1 year' + a.forecast_period) + a.forecast_period AS forecast_period,
   a.analysis_time + a.forecast_period AS orig_forecast_time,
-  date_trunc('year', a.analysis_time + a.forecast_period) + (a.analysis_time - date_trunc('year', a.analysis_time + a.forecast_period)) + a.forecast_period AS forecast_time,
+  date_trunc('year', a.analysis_time + a.forecast_period) + (a.analysis_time - date_trunc('year', a.analysis_time + a.forecast_period)) + (extract(year from current_date) - 2019) * interval '1 year' + a.forecast_period AS forecast_time,
   a.file_location,
   a.file_server,
   a.forecast_type_id,
@@ -59,6 +60,45 @@ WHERE
   date_trunc('year', a.analysis_time + a.forecast_period) >= '2019-01-01 00:00:00'::timestamptz
   AND
   date_trunc('year', a.analysis_time + a.forecast_period) < '2020-01-01 00:00:00'::timestamptz
+)
+SELECT
+  producer_id,
+  producer_name,
+  analysis_time,
+  geometry_id,
+  geometry_name,
+  param_id,
+  param_name,
+  level_id,
+  level_name,
+  level_value,
+  level_value2,
+  orig_analysis_time,
+  orig_forecast_period,
+  forecast_period,
+  orig_forecast_time,
+  CASE
+    WHEN extract(month FROM forecast_time) = 2 AND extract(day FROM forecast_time) = 29 THEN
+    forecast_time + interval '1 day'
+    ELSE
+    forecast_time
+  END AS forecast_time,
+  file_location,
+  file_server,
+  forecast_type_id,
+  forecast_type_name,
+  forecast_type_value,
+  file_format_id,
+  file_format_name,
+  file_protocol_id,
+  file_protocol_name,
+  message_no,
+  byte_offset,
+  byte_length,
+  last_updater,
+  last_updated
+FROM
+  raw
 ;
 
 GRANT SELECT ON grid_era5mta_ssv TO public;
